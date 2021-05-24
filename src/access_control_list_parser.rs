a8a887e05c28419be9ab429a1de9f21d144bddbe
Nikolaus Thümmel <commit.nthuemmel@ntcomputer.de>
2021-05-24 15:51:19 +0200

    Add access control list parsing tests

--- src/access_control_list_parser.rs
+++ src/access_control_list_parser.rs
@@ -13,7 +13,10 @@ use std::str::FromStr;
 
 pub fn parse_file(file_path: &Path) -> anyhow::Result<AccessControlTree> {
     let file = File::open(file_path)?;
-    let reader = BufReader::new(file);
+    parse_input(BufReader::new(file))
+}
+
+fn parse_input(reader: impl BufRead) -> anyhow::Result<AccessControlTree> {
     let mut tree_builder = AccessControlTreeBuilder::new();
 
     for (line_number, line) in reader.lines().enumerate() {
@@ -123,4 +126,151 @@ fn insert_act_entry(
     entry_modifier(tree_builder.entry(client_subnet));
 
     Ok(())
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+    use crate::access_control_tree::{ACTDomain, ACTSocketAddress, ACTSubnet, AccessControlTree};
+    use crate::protocol::Protocol;
+    use ipnet::IpNet;
+    use maplit::hashmap;
+    use std::net::IpAddr;
+    use std::str::FromStr;
+
+    fn parse(input: &str) -> AccessControlTree {
+        parse_input(input.as_bytes()).unwrap()
+    }
+
+    #[test]
+    fn exhaustive_example() {
+        let input = r#"
+# Some comment
+192.168.4.0/24 ~> mail.local
+192.168.4.0/24 -> r3.o.lencr.org:TCP:80					# Let's Encrypt OCSP responder
+2001:0db8:85a3:0000:0000:8a2e:0370:7334 -> example.com:TCP:22
+
+10.34.3.3 -> example.local:UDP:3478
+10.3.4.5 ~> *
+192.168.1.1 -> service.local:tcp:443
+192.168.1.1 -> service.local:udp:333
+   # huh
+"#;
+        let act = parse(input);
+
+        let expected_entries = hashmap! {
+            IpNet::from_str("192.168.4.0/24").unwrap() => ACTSubnet {
+                allow_all_dns_queries: false,
+                domain_name_map: hashmap! {
+                    "mail.local".to_string() => ACTDomain {
+                        allow_all_dns_questions: true,
+                        allowed_destination_sockets: vec![],
+                    },
+                    "r3.o.lencr.org".to_string() => ACTDomain {
+                        allow_all_dns_questions: false,
+                        allowed_destination_sockets: vec![
+                            ACTSocketAddress {
+                                protocol: Protocol::Tcp,
+                                port: 80,
+                            }
+                        ]
+                    }
+                },
+            },
+            IpAddr::from_str("2001:0db8:85a3:0000:0000:8a2e:0370:7334").unwrap().into() => ACTSubnet {
+                allow_all_dns_queries: false,
+                domain_name_map: hashmap! {
+                    "example.com".to_string() => ACTDomain {
+                        allow_all_dns_questions: false,
+                        allowed_destination_sockets: vec![
+                            ACTSocketAddress {
+                                protocol: Protocol::Tcp,
+                                port: 22,
+                            }
+                        ]
+                    }
+                },
+            },
+            IpAddr::from_str("10.34.3.3").unwrap().into() => ACTSubnet {
+                allow_all_dns_queries: false,
+                domain_name_map: hashmap! {
+                    "example.local".to_string() => ACTDomain {
+                        allow_all_dns_questions: false,
+                        allowed_destination_sockets: vec![
+                            ACTSocketAddress {
+                                protocol: Protocol::Udp,
+                                port: 3478,
+                            }
+                        ]
+                    }
+                },
+            },
+            IpAddr::from_str("10.3.4.5").unwrap().into() => ACTSubnet {
+                allow_all_dns_queries: true,
+                domain_name_map: hashmap! {},
+            },
+            IpAddr::from_str("192.168.1.1").unwrap().into() => ACTSubnet {
+                allow_all_dns_queries: false,
+                domain_name_map: hashmap! {
+                    "service.local".to_string() => ACTDomain {
+                        allow_all_dns_questions: false,
+                        allowed_destination_sockets: vec![
+                            ACTSocketAddress {
+                                protocol: Protocol::Tcp,
+                                port: 443,
+                            },
+                            ACTSocketAddress {
+                                protocol: Protocol::Udp,
+                                port: 333,
+                            }
+                        ]
+                    }
+                },
+            },
+        };
+
+        assert_eq!(act.into_map(), expected_entries);
+    }
+
+    #[test]
+    #[should_panic(
+        expected = "Invalid destination 'dest.local:TCP': Expected a 'domain:protocol:port' triple"
+    )]
+    fn target_triple_too_short() {
+        parse("192.168.1.1 -> dest.local:TCP");
+    }
+
+    #[test]
+    #[should_panic(
+        expected = "Invalid destination 'dest.local:TCP:80:80': Expected a 'domain:protocol:port' triple"
+    )]
+    fn target_triple_too_long() {
+        parse("192.168.1.1 -> dest.local:TCP:80:80");
+    }
+
+    #[test]
+    #[should_panic(expected = "Unknown protocol 'SLURP'")]
+    fn unknown_protocol() {
+        parse("192.168.1.1 -> dest.local:SLURP:80");
+    }
+
+    #[test]
+    #[should_panic(expected = "Invalid port 'foo'")]
+    fn invalid_port() {
+        parse("192.168.1.1 -> dest.local:TCP:foo");
+    }
+
+    #[test]
+    #[should_panic(
+        expected = "Invalid format: Expected '[client IP address or subnet (CIDR)] -> [domain]:[protocol]:[port]' or '[client IP address or subnet (CIDR)] ~> [domain]'"
+    )]
+    fn invalid_format() {
+        parse("192.168.1.1 |> dest.local:TCP:80");
+    }
+
+    #[test]
+    #[should_panic(expected = "invalid IP address syntax")]
+    fn invalid_ip() {
+        parse("xfoo -> dest.local:TCP:80");
+    }
 }
--- src/access_control_tree.rs
+++ src/access_control_tree.rs
@@ -3,23 +3,24 @@ use ipnet::IpNet;
 use std::collections::HashMap;
 use std::net::IpAddr;
 
+#[derive(Debug, PartialEq)]
 pub struct AccessControlTree {
     entries: Vec<(IpNet, ACTSubnet)>,
 }
 
-#[derive(Debug, Default)]
+#[derive(Debug, Default, PartialEq)]
 pub struct ACTSubnet {
     pub allow_all_dns_queries: bool,
     pub domain_name_map: HashMap<String, ACTDomain>,
 }
 
-#[derive(Debug, Default)]
+#[derive(Debug, Default, PartialEq)]
 pub struct ACTDomain {
     pub allow_all_dns_questions: bool,
     pub allowed_destination_sockets: Vec<ACTSocketAddress>,
 }
 
-#[derive(Debug, Clone, Copy)]
+#[derive(Debug, Clone, Copy, PartialEq)]
 pub struct ACTSocketAddress {
     pub protocol: Protocol,
     pub port: u16,
@@ -36,6 +37,11 @@ impl AccessControlTree {
             .filter(|(net, _)| net.contains(&client_ip_address))
             .map(|(_, v)| v)
             .collect()
+    }
+
+    #[cfg(test)]
+    pub fn into_map(self) -> HashMap<IpNet, ACTSubnet> {
+        self.entries.into_iter().collect()
     }
 }
 
