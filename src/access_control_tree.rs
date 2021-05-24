b391121347336fcca78e7f5b671c2d704296afdd
Nikolaus Thümmel <commit.nthuemmel@ntcomputer.de>
2021-05-24 20:28:31 +0200

    Allow subdomain wildcard filtering

--- src/access_control_tree.rs
+++ src/access_control_tree.rs
@@ -1,4 +1,5 @@
 use crate::protocol::Protocol;
+use anyhow::bail;
 use ipnet::IpNet;
 use std::collections::HashMap;
 use std::net::IpAddr;
@@ -11,13 +12,26 @@ pub struct AccessControlTree {
 #[derive(Debug, Default, PartialEq)]
 pub struct ACTSubnet {
     pub allow_all_dns_queries: bool,
-    pub domain_name_map: HashMap<String, ACTDomain>,
+    pub domains: Vec<ACTDomain>,
 }
 
-#[derive(Debug, Default, PartialEq)]
+#[derive(Debug, PartialEq)]
 pub struct ACTDomain {
+    pub name: DomainNamePattern,
     pub allow_all_dns_questions: bool,
     pub allowed_destination_sockets: Vec<ACTSocketAddress>,
+}
+
+#[derive(Debug, PartialEq)]
+pub enum DomainNamePattern {
+    Any,
+    Exact {
+        fqdn: String,
+    },
+    AllSubdomainsOf {
+        /// Must start with a dot
+        fqdn_tail: String,
+    },
 }
 
 #[derive(Debug, Clone, Copy, PartialEq)]
@@ -42,6 +56,54 @@ impl AccessControlTree {
     #[cfg(test)]
     pub fn into_map(self) -> HashMap<IpNet, ACTSubnet> {
         self.entries.into_iter().collect()
+    }
+}
+
+impl ACTSubnet {
+    pub fn get_or_create_domain_entry(&mut self, name: DomainNamePattern) -> &mut ACTDomain {
+        // See https://stackoverflow.com/questions/58249193/how-to-find-or-insert-into-a-vec-in-rust
+        // for why using indices is necessary here
+        for i in 0..self.domains.len() {
+            if self.domains[i].name == name {
+                return &mut self.domains[i];
+            }
+        }
+
+        self.domains.push(ACTDomain {
+            name,
+            allow_all_dns_questions: false,
+            allowed_destination_sockets: Vec::new(),
+        });
+        self.domains.last_mut().unwrap()
+    }
+}
+
+impl DomainNamePattern {
+    pub fn parse(input: &str) -> anyhow::Result<Self> {
+        if input == "*" {
+            Ok(Self::Any)
+        } else if input.starts_with("*.") {
+            Ok(Self::AllSubdomainsOf {
+                fqdn_tail: input[1..].to_string(),
+            })
+        } else if input.contains('*') {
+            bail!(
+                "Wildcards (*) may only appear at the beginning \
+                 in place of a subdomain (e.g. '*.example.local')"
+            );
+        } else {
+            Ok(Self::Exact {
+                fqdn: input.to_string(),
+            })
+        }
+    }
+
+    pub fn matches(&self, against_fqdn: &str) -> bool {
+        match self {
+            Self::Any => true,
+            Self::Exact { fqdn } => fqdn == against_fqdn,
+            Self::AllSubdomainsOf { fqdn_tail } => against_fqdn.ends_with(fqdn_tail),
+        }
     }
 }
 
